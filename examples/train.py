e019fd5562f04f0cea977e5decaf3ab110ee3b19
hp0404 <hryhorii.pavlenko@gmail.com>
2021-04-12 15:57:12 +0300

    replaced Options callbacks with Enum - Choices; resolves #6

--- examples/train.py
+++ examples/train.py
@@ -1,9 +1,24 @@
 import json
 import typer
+from enum import Enum
 from pathlib import Path
 from top2vec import Top2Vec
 
 app = typer.Typer(name="train", add_completion=False, help="This is a training script.")
+
+
+class Model(str, Enum):
+    DOC2VEC = "doc2vec"
+    USE = "universal-sentence-encoder"
+    USEM = "universal-sentence-encoder-multilingual"
+    DBMC = "distiluse-base-multilingual-cased"
+
+
+class Speed(str, Enum):
+    TEST_LEARN = "test-learn"
+    FAST_LEARN = "fast-learn"
+    LEARN = "learn"
+    DEEP_LEARN = "deep-learn"
 
 
 def check_parent_exists(path):
@@ -13,83 +28,53 @@ def check_parent_exists(path):
     return path
 
 
-def check_embedding_model(model):
-    valid_models = (
-        "doc2vec",
-        "universal-sentence-encoder",
-        "universal-sentence-encoder-multilingual",
-        "distiluse-base-multilingual-cased",
-    )
-    model = model.lower().strip()
-    if model not in valid_models:
-        typer.echo(f"{model=} is not supported.")
-        typer.echo(
-            f"The valid string options for 'embedding_model' are: {', '.join(valid_models)}"
-        )
-        raise typer.Exit(code=1)
-    return model
-
-
-def check_speed(speed):
-    valid_speeds = ("test-learn", "fast-learn", "learn", "deep-learn")
-    speed = speed.lower().strip()
-    if speed not in valid_speeds:
-        typer.echo(f"{speed=} is not supported.")
-        typer.echo(f"speed parameter needs to be one of: {', '.join(valid_speeds[1:])}")
-        raise typer.Exit(code=1)
-    return speed
-
-
 @app.command()
 def train(
-    data: Path = typer.Argument(
-        ...,
-        help="JSON file containing documents for training.",
-        exists=True
+    load: Path = typer.Argument(
+        ..., help="Path to a file containing documents for training.", exists=True
     ),
-    model: Path = typer.Argument(
-        ..., help="Output path for model.", callback=check_parent_exists
+    save: Path = typer.Argument(
+        ..., help="Output path for the model.", callback=check_parent_exists
     ),
-    embedding_model: str = typer.Argument(
-        "doc2vec",
+    embedding_model: Model = typer.Option(
+        Model.DOC2VEC,
         help="Which model is used to generate the document and word embeddings.",
-        callback=check_embedding_model,
     ),
-    speed: str = typer.Option(
-        "learn", help="How fast the model takes to train", callback=check_speed
+    training_speed: Speed = typer.Option(
+        Speed.LEARN, help="How fast the model takes to train"
     ),
     workers: int = typer.Option(
         128, help="The amount of worker threads to be used in training the model"
     ),
-    preprocess_data: bool = typer.Option(True, is_flag=True, help="Remove empty docs."),
 ):
     """Train Top2Vec algorithm."""
     typer.echo("Loading data...")
-    with open(data, "r", encoding="utf-8") as f:
+    with open(load, "r", encoding="utf-8") as f:
         data = json.load(f)
-    if preprocess_data:
-        docs = [
-            doc.get("fulltext", "")
-            for doc in data
-            if not doc.get("fulltext", "").startswith("Not available.")
-        ]
-    else:
-        docs = [doc.get("fulltext", "") for doc in data]
-    if embedding_model == "doc2vec":
+
+    docs = [
+        doc.get("fulltext", "")
+        for doc in data
+        if not doc.get("fulltext", "").startswith("Not available.")
+    ]
+
+    speed = training_speed.value
+    model = embedding_model.value
+    if model == "doc2vec":
         typer.echo(
-            f"Training the model with following parameters: {embedding_model=}, {speed=}, {workers=}"
+            f"Training the model with following parameters: {model=}, {speed=}, {workers=}"
         )
         t2v = Top2Vec(
             documents=docs,
-            embedding_model=embedding_model,
+            embedding_model=model,
             speed=speed,
             workers=workers,
         )
     else:
-        typer.echo(f"Training the model with following parameters: {embedding_model=}")
-        t2v = Top2Vec(documents=docs, embedding_model=embedding_model)
-    typer.echo(f"Saving the model to {model}")
-    t2v.save(model)
+        typer.echo(f"Training the model with following parameters: {model=}")
+        t2v = Top2Vec(documents=docs, embedding_model=model)
+    typer.echo(f"Saving the model to {save}")
+    t2v.save(save)
 
 
 if __name__ == "__main__":
